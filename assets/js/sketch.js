4bdf1d1a90919767c0fb0996b055c344e27b02d4
Kevin Malekera <r0900654@student.thomasmore.be>
2024-05-25 23:08:08 +0200

    Added global variables and setup function

--- assets/js/sketch.js
+++ assets/js/sketch.js
@@ -1,2 +1,12 @@
 'use strict';
 
+// Global variables
+let video;
+let poseNet, ball;
+let poses = [];
+
+const setup = () => {
+  console.log('Conent loaded');
+}
+
+window.addEventListener('DOMContentLoaded', setup);
