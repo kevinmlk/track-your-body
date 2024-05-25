69e221d095b477e357cadc61d43d40530f4e4e73
Kevin Malekera <r0900654@student.thomasmore.be>
2024-05-25 23:39:46 +0200

    Added function that draws the skeleton

--- assets/js/sketch.js
+++ assets/js/sketch.js
@@ -8,9 +8,11 @@ let poses = [];
 let modelLoaded = false;
 
 function setup() {
+  // Create canvas
   createCanvas(500, 500);
   video = createCapture(VIDEO);
   video.size(width, height);
+  // Create poseNet method
   poseNet = ml5.poseNet(video, modelReady);
   poseNet.on("pose", function (results) {
     poses = results;
@@ -18,14 +20,45 @@ function setup() {
   video.hide();
 }
 
-// Function that tells that the modal is ready
+// Function that tells that the model is ready
 function modelReady() {
   document.querySelector("#status").textContent = "Model Loaded";
   modelLoaded = true;
 }
 
+// Function that draws when the model is ready
 function draw() {
   if (modelLoaded) {
     image(video, 0, 0, width, height);
+  }
+}
+
+// Function that draws the skeleton
+function drawSkeleton() {
+  // Detected skeletons loop
+  for (let i = 0; i < poses.length; i++) {
+    let skeleton = poses[i].skeleton;
+    // Body connections loop
+    for (let j = 0; j < skeleton.length; j++) {
+      let partA = skeleton[j][0];
+      let partB = skeleton[j][1];
+      
+      // Limbs calculations
+      spriteX = (partA.position.x + partB.position.x) / 2;
+      spriteY = (partA.position.y + partB.position.y) / 2;
+      distance = dist(
+        partA.position.x,
+        partA.position.y,
+        partB.position.x,
+        partB.position.y
+      );
+      angle = atan(
+        (partA.position.y - partB.position.y) /
+          (partA.position.x - partB.position.x)
+      );
+      // Limbs
+      sprite = new limbs.Sprite(spriteX, spriteY, distance, width * 0.015);
+      sprite.rotation = angle;
+    }
   }
 }
