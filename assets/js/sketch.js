1f80bde693a7ad6c66d442320b09e40ade885cba
Kevin Malekera <r0900654@student.thomasmore.be>
2024-05-25 23:10:39 +0200

    Added modelLoaded function and variable

--- assets/js/sketch.js
+++ assets/js/sketch.js
@@ -2,11 +2,19 @@
 
 // Global variables
 let video;
-let poseNet, ball;
+let poseNet;
+let ball;
 let poses = [];
+let modalLeaded = false;
 
 const setup = () => {
   console.log('Conent loaded');
+}
+
+// Function that tells that the modal is ready
+const modalReady = () => {
+  console.log('modal ready');
+  modalLeaded = true;
 }
 
 window.addEventListener('DOMContentLoaded', setup);
